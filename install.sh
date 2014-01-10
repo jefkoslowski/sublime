35d7860e15eb4944a3fb740e63cfa03860f4d4bf
Jeferson Koslowski <jef.koslowski@gmail.com>
2014-01-10 00:26:10 -0200

    Added installation script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+SUBLIME_CONFIG=~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/
+SUBLIME_THEME=~/Library/Application\ Support/Sublime\ Text\ 2/Packages/Color\ Scheme\ -\ User/
+
+cd "$(dirname "${BASH_SOURCE}")"
+git pull origin master
+function doIt() {
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "install.sh" \
+		--exclude "README.md" --exclude Color\ Scheme\ -\ User/ -av --no-perms . "$SUBLIME_CONFIG"
+
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "install.sh" \
+		--exclude "README.md" -av --no-perms Color\ Scheme\ -\ User/ "$SUBLIME_THEME"
+}
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt
+	fi
+fi
+unset doIt
